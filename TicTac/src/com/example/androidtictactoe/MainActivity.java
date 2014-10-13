ecc47bb07e7d8737ef59425c5e07a7ead885a46c
Asiel Alvarez <Asiel Alvarez@AbelAAlvarez-PC.attlocal.net>
2014-10-13 10:52:19 -0400

    Done, missing decoration and symbol sizes

--- TicTac/src/com/example/androidtictactoe/MainActivity.java
+++ TicTac/src/com/example/androidtictactoe/MainActivity.java
@@ -63,50 +63,104 @@ public class MainActivity extends Activity {
         }       
         
         
-        	if((button.getId()+"").equals("2131230722") && button.getText().equals("")){
+        	if((button.getId()+"").equals("2131230722") && button.getText().equals("") && win){
         		button1 = (Button)v;
         		button1.setText(symbol);
-        		counter++;
+        		counter++;        		
+        		if(symbol.equals("X")){
+        			button1.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button1.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230723") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230723") && button.getText().equals("") && win){
         		button2 = (Button)v;
         		button2.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button2.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button2.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230724") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230724") && button.getText().equals("") && win){
         		button3 = (Button)v;
         		button3.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button3.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button3.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230725") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230725") && button.getText().equals("") && win){
         		button4 = (Button)v;
         		button4.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button4.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button4.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230726") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230726") && button.getText().equals("") && win){
         		button5 = (Button)v;
         		button5.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button5.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button5.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230727") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230727") && button.getText().equals("") && win){
         		button6 = (Button)v;
         		button6.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button6.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button6.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230728") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230728") && button.getText().equals("") && win){
         		button7 = (Button)v;
         		button7.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button7.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button7.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230729") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230729") && button.getText().equals("") && win){
         		button8 = (Button)v;
         		button8.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button8.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button8.setTextColor(Color.RED);
+        		}
         	}
-        	else if((button.getId()+"").equals("2131230730") && button.getText().equals("")){
+        	else if((button.getId()+"").equals("2131230730") && button.getText().equals("") && win){
         		button9 = (Button)v;
         		button9.setText(symbol);
         		counter++;
+        		if(symbol.equals("X")){
+        			button9.setTextColor(Color.BLUE);
+        		}
+        		else{
+        			button9.setTextColor(Color.RED);
+        		}
         	}
         	
         	if(counter >= 5){
@@ -114,10 +168,41 @@ public class MainActivity extends Activity {
         	}
         
         
-       // }
+// }
 //        EditText user = (EditText)findViewById(R.id.username);
 //        EditText pass = (EditText)findViewById(R.id.password);
 //        error = (TextView)findViewById(R.id.error);
+	}
+	
+	public void newGame(View v){
+		
+		button1 = (Button)findViewById(R.id.button1);
+		button2 = (Button)findViewById(R.id.button2);
+		button3 = (Button)findViewById(R.id.button3);
+		button4 = (Button)findViewById(R.id.button4);
+		button5 = (Button)findViewById(R.id.button5);
+		button6 = (Button)findViewById(R.id.button6);
+		button7 = (Button)findViewById(R.id.button7);
+		button8 = (Button)findViewById(R.id.button8);
+		button9 = (Button)findViewById(R.id.button9);
+		infoText = (TextView)findViewById(R.id.text);
+		
+    	button1.setText("");
+		button2.setText("");
+		button3.setText("");
+		button4.setText("");
+		button5.setText("");
+		button6.setText("");
+		button7.setText("");
+		button8.setText("");
+		button9.setText("");
+		
+		
+		infoText.setText("Game On!");
+		infoText.setTextColor(Color.BLACK);
+				
+		counter = 0;
+		win = true;
 	}
 	
 	private void checkWinner(){	
@@ -133,7 +218,7 @@ public class MainActivity extends Activity {
 			(button1.getText().equals(button5.getText()) && button1.getText().equals(button9.getText()) && !button1.getText().equals("")) ||
 			(button7.getText().equals(button5.getText()) && button7.getText().equals(button3.getText()) && !button7.getText().equals(""))) {
 			
-			if(counter % 2 == 1){
+			if(counter % 2 == 0){
 				infoText.setText("O - wins!");
 				//infoText.setForeground(Color.RED);
 				infoText.setTextColor(Color.RED);
