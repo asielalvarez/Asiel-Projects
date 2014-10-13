1a5b351f4fa52e9f111ccd96dd6480dfe6ff5217
Asiel Alvarez <Asiel Alvarez@AbelAAlvarez-PC>
2014-10-12 22:57:48 -0400

    new names added to buttons and label

--- TicTac/gen/com/example/androidtictactoe/R.java
+++ TicTac/gen/com/example/androidtictactoe/R.java
@@ -26,19 +26,19 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f08000d;
-        public static final int button1=0x7f08000a;
-        public static final int button10=0x7f08000b;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080003;
-        public static final int button4=0x7f080007;
-        public static final int button5=0x7f080004;
-        public static final int button6=0x7f080006;
+        public static final int button1=0x7f080002;
+        public static final int button10=0x7f08000c;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f080006;
+        public static final int button6=0x7f080007;
         public static final int button7=0x7f080008;
         public static final int button8=0x7f080009;
-        public static final int button9=0x7f080005;
+        public static final int button9=0x7f08000a;
         public static final int gridLayout1=0x7f080001;
+        public static final int infoText=0x7f08000b;
         public static final int linearLayout1=0x7f080000;
-        public static final int textView1=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -47,9 +47,20 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int NewGame=0x7f05000d;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int button1=0x7f050004;
+        public static final int button2=0x7f050005;
+        public static final int button3=0x7f050006;
+        public static final int button4=0x7f050007;
+        public static final int button5=0x7f050008;
+        public static final int button6=0x7f050009;
+        public static final int button7=0x7f05000a;
+        public static final int button8=0x7f05000b;
+        public static final int button9=0x7f05000c;
         public static final int hello_world=0x7f050001;
+        public static final int infoText=0x7f050003;
     }
     public static final class style {
         /** 
